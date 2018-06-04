861a128ee680d8b784fac8e439c4402aadd82c72
Wang <Daniel.Wang@contractor.conocophillips.com>
2018-06-04 14:05:29 +0800

    consoleapp2

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("5 + 3 = " + (5 + 3));
+            Console.WriteLine("5 - 3 = " + (5 - 3));
+            Console.WriteLine("5 * 3 = " + (5 * 3));
+            Console.WriteLine("5 / 3 = " + (5 / 3));
+
+            int i = 0;
+            Console.WriteLine("i++ = " + (i++));
+            Console.WriteLine("++i = " + (++i));
+            Console.WriteLine("i-- = " + (i--));
+            Console.WriteLine("--i = " + (--i));
+
+            Console.WriteLine("i += 3 " + (i += 3));
+            Console.WriteLine("i -= 2 " + (i -= 2));
+            Console.WriteLine("i *= 2 " + (i *= 2));
+            Console.WriteLine("i /= 2" + (i /= 2));
+            Console.WriteLine("i %= 2" + (i %= 2));
+
+
+
+        }
+    }
+}
